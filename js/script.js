06886f090bf139122f203e4a564b25d7c3d7dab7
Simounet <contact@simounet.net>
2014-02-23 19:13:03 +0100

    First toggle view handling

--- js/script.js
+++ js/script.js
@@ -5,20 +5,62 @@ $('document').ready(function(){
     });
 });
 
-function switchView(e) {
-   var newRssOrSiteView = + ! e.data('view');
+function switchView(element) {
+    var newRssOrSiteView = + ! element.data('view'),
+        entry = element.parents('.js-feed__entry');
 
     $.ajax({
         url: "./plugins/LeedRSSOrSiteView/update_view.php",
-        data:{ id: 176, view: newRssOrSiteView },
+        data:{ id: entry.data( 'feed-id' ), view: newRssOrSiteView },
     })
         .done(function() {
-            var newRssOrSiteViewText = ( newRssOrSiteView == 1 ) ? 'SITE_VIEW' : 'RSS_VIEW';
-            e.data('view', newRssOrSiteView)
+            var newRssOrSiteViewText = ( newRssOrSiteView == 1 ) ?  'RSS_VIEW' : 'SITE_VIEW';
+            element.data('view', newRssOrSiteView)
                 .html( _t( newRssOrSiteViewText ) );
+            
+            toggleView( entry.find('.js-article__content'), newRssOrSiteView );
             
         })
         .fail(function() {
             alert( "error" );
         });
 }
+
+// Create the site iframe
+function toggleWebsite( element ) {
+    var entry = element.parents('.js-feed__entry'),
+        articleContent = entry.find('.js-article__content');
+    if( articleContent.not(':has(iframe)').length ) {
+        entry.addClass('article__content--website js-website');
+        entry.find('.js-article__content--rss').addClass('hidden');
+        if( entry.hasClass('js-focus') )
+            jQuery('<iframe frameborder="0" src="' + articleContent.data('article-url') + '" style="width: 100%; height: 100%;" />').appendTo( articleContent );
+    } else {
+        articleContent.children('iframe').remove();
+    }
+}
+
+// [todo] - Get RSS content instead of this show/hide trick
+function toggleRSS( element ) {
+    var entry = element.parents('.js-feed__entry'),
+        content = entry.find('.js-article__content'),
+        contentIframe = content.find( 'iframe' );
+    
+    if( contentIframe.length > 0 )
+        contentIframe.remove();
+    
+    content.find( '.js-article__content--rss' ).removeClass('hidden');
+    entry.removeClass('article__content--website js-website');
+}
+
+// [todo] - Create a general object to avoid these repetitive node switches
+function toggleView( element, view ) {
+    $( '[data-feed-id="' + element.parents('.js-feed__entry').data('feed-id') + '"]' ).each(function() {
+        if( view == 1 ) {
+            toggleWebsite( $(this).children() );
+        } else {
+            toggleRSS( $(this).children() );
+        }
+    });
+}
+
